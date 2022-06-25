d8ef7363a08c2423e65c607f87a9f3600855809b
U-235-92 <a745296@yandex.ru>
2022-06-25 13:00:00 +0400

    Level 2 Lesson 3

--- src/main/java/aq/koptev/level2/lesson3/Main.java
+++ src/main/java/aq/koptev/level2/lesson3/Main.java
@@ -10,5 +10,17 @@ public class Main {
         WordParser wordParser = new WordParser(text);
         wordParser.printUniqueWords();
         wordParser.printUniqueWordsCountStatistic();
+
+        System.out.println();
+
+        TelephoneBook telephoneBook = new TelephoneBook();
+        telephoneBook.add("Коптев", "88009995522");
+        telephoneBook.add("Коптев", "89008887744");
+        telephoneBook.add("Коптев", "a89008887744");
+        telephoneBook.add("Коптев", "890088877ff44");
+        telephoneBook.add("Коптев", "89008887744.");
+        telephoneBook.add("Иванов", "89755533214");
+        telephoneBook.add("Иванов", "89755533214й");
+        telephoneBook.printContactInfo("Коптев");
     }
 }
